2de82e93d2a0b104a73efd293fd200182247b529
ckf10000 <ckf10000@sina.com>
2024-04-12 11:17:49 +0800

    first

---
+++ tests/test_app_ui_services.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+# -----------------------------------------------------------------------------------------------------------------------
+# ProjectName:  smartIssueTickets
+# FileName:     test_app_services.py
+# Description:  TODO
+# Author:       ckf10000
+# CreateDate:   2024/04/01 11:14:51
+# Copyright ©2011-2024. Hunan xyz Company limited. All rights reserved.
+# -----------------------------------------------------------------------------------------------------------------------
+"""
+from apps.domain.services.app_ui_services import CtripAppService
+
+
+def test_select_special_flight():
+    app = CtripAppService()
+    app.start()
+    app.select_special_flight(flight="MF8266")
+
+
+def test_select_insecure():
+    app = CtripAppService()
+    app.start()
+    app.select_insecure()
+
+
+def test_get_tickect_actual_amount():
+    app = CtripAppService()
+    app.start()
+    app.get_tickect_actual_amount()
+
+
+def test_get_tickect_deduction_amount():
+    app = CtripAppService()
+    app.start()
+    app.get_tickect_deduction_amount()
+
+
+def test_get_pending_trip_order():
+    app = CtripAppService()
+    app.start()
+    app.get_pending_trip_order()
+
+
+def test_close_important_trip_guidelines():
+    app = CtripAppService()
+    app.start()
+    app.close_important_trip_guidelines()
+
+
+def test_get_flight_ticket_with_order_id():
+    app = CtripAppService()
+    app.start()
+    print(app.get_flight_ticket_with_order_id())
+
+
+def test_get_flight_ticket_with_itinerary_id():
+    app = CtripAppService()
+    app.start()
+    print(app.get_flight_ticket_with_itinerary_id())
+
+
+def test_select_more_payment():
+    app = CtripAppService()
+    app.start()
+    app.select_more_payment()
+
+
+def test_enter_payment_pass():
+    app = CtripAppService()
+    app.start()
+    app.enter_payment_pass(payment_pass="0123456789")
+
+
+def test_select_search_result_first_city():
+    app = CtripAppService()
+    app.start()
+    app.select_search_result_first_city(select_value="PEK")
+
+
+def test_check_user_login():
+    app = CtripAppService()
+    app.start()
+    app.check_user_login(username="18600440822", password="ca161022")
+
+
+def test_close_coupon_dialog():
+    app = CtripAppService()
+    app.start()
+    app.close_coupon_dialog()
+
+
+def test_expand_order_detail():
+    app = CtripAppService()
+    app.start()
+    app.expand_order_detail()
+
+
+def test_touch_order_detail():
+    app = CtripAppService()
+    app.start()
+    app.touch_order_detail()
+
+
+def test_touch_passenger_card_type():
+    app = CtripAppService()
+    app.start()
+    app.touch_passenger_card_type()
+
+
+def test_select_passenger_card_type():
+    app = CtripAppService()
+    app.start()
+    app.select_passenger_card_type(card_type="身份证")
+
+
+if __name__ == "__main__":
+    # test_select_special_flight()
+    # test_select_insecure()
+    # test_get_tickect_actual_amount()
+    # test_get_tickect_deduction_amount()
+    # test_get_pending_trip_order()
+    # test_close_important_trip_guidelines()
+    # test_get_flight_ticket_with_order_id()
+    # test_get_flight_ticket_with_itinerary_id()
+    # test_select_more_payment()
+    # test_enter_payment_pass()
+    # test_select_search_result_first_city()
+    # test_check_user_login()
+    # test_close_coupon_dialog()
+    # test_expand_order_detail()
+    # test_touch_order_detail()
+    test_touch_passenger_card_type()
+    test_select_passenger_card_type()
---
+++ tests/test_booking_services.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+# -----------------------------------------------------------------------------------------------------------------------
+# ProjectName:  smartIssueTickets
+# FileName:     test_booking_services.py
+# Description:  TODO
+# Author:       ckf10000
+# CreateDate:   2024/04/01 10:11:15
+# Copyright ©2011-2024. Hunan xyz Company limited. All rights reserved.
+# -----------------------------------------------------------------------------------------------------------------------
+"""
+from apps.application.services.booking_services import booking_flight_ser
+
+
+def test_booking_ctrip_app_special_flight_ticket():
+    booking_flight_ser.booking_ctrip_app_special_flight_ticket(
+        departure_time="2024-04-13 21:50",
+        departure_city="NKG",
+        departure_city_name="南京",
+        arrive_time="2024-04-13 23:30",
+        arrive_city="CSX",
+        arrive_city_name="长沙",
+        flight="MF8036",
+        passenger="李成浩",
+        card_id="321284199803018015",
+        card_type="身份证",
+        pre_sale_amount="240.00",
+        payment_pass="901127",
+        internal_phone="18569520328",
+        passenger_phone="18261063386",
+        age_stage="成人",
+        pre_order_id="2891160",
+        ctrip_username="18600440822",
+        user_pass="ca161022",
+        payment_method="浦发银行储蓄卡(7397)"
+    )
+
+
+if __name__ == "__main__":
+    test_booking_ctrip_app_special_flight_ticket()
